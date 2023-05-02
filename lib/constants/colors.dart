eef4fbcda1364bf6c15f6af6312a122b6bd4c55d
supremoods <80272399+supremoods@users.noreply.github.com>
2023-05-02 21:49:11 +0800

    updated color palette

---
+++ lib/constants/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xFF6360FF);
+const Color secondary = Color(0xFFFF8181);
+const Color success = Color(0xFF7DC579);
+const Color warning = Color(0xFFFFC960);
+const Color error = Color(0xFFFF6D9F);
+const Color default_ = Color(0xFF292B2D);
+
+
+const Color light_100 = Color(0xFFFFFFFF);
+const Color light_200 = Color(0xFFF1F1FA);
+const Color light_300 = Color(0xFF91919F);
+
+const Color dark_100 = Color(0xFF161719);
+const Color dark_200 = Color(0xFF212325); 
+const Color dark_300 = Color.fromARGB(61, 243, 243, 248);
